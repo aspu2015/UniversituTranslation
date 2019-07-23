fc149186ecd5470e9a723a953891cfd1cf85fa2f
Alexander <travov@asu.edu.ru>
2019-07-23 20:03:53 +0400

    finaly

--- public/js/langs/info_translations.js
+++ public/js/langs/info_translations.js
@@ -1,19 +1,7 @@
-//var langs = {};
-// function findGetParameter(parameterName) {
-//     var result = null,
-//         tmp = [];
-//     location.search
-//         .substr(1)
-//         .split("&")
-//         .forEach(function (item) {
-//           tmp = item.split("=");
-//           if (tmp[0] === parameterName) result = decodeURIComponent(tmp[1]);
-//         });
-//     return result;
-// }
+var langs = {};
 
 function onOptionClick(langName){
-    console.log('Языки: ',langs);
+    console.log(1);
     for(var i =0; i < langs.length; i++){
         let obj = langs[i];
         if(obj.langName == langName){
@@ -22,38 +10,30 @@ function onOptionClick(langName){
             ($('#textBody')[0]).insertAdjacentHTML('beforeend',obj.text);
         }
     }
-
 }
 $(document).ready(function(){
 
     
 
     $.ajax({
-<<<<<<< HEAD
         url: "/api/langs?id=99999"
-=======
-        url: "/api/langs?id=9999"
->>>>>>> 98ed5fb4c9d7235a18eeedeb0c001d4f81d31f60
     }).done(function(data) {
         var translations = JSON.parse(data);
         langs = translations;
         let firstValue = null;
         for(var i = 0; i < translations.length; i++){
+            
             let langName = translations[i].langName;
             let picturePath = translations[i].picturePath;
-            $('#langs').append($('<div value="'+langName+'"><img src="'+picturePath+'">'+langName+'</div>'));
             $('#webmenu').append($('<option value="'+langName+'" data-image="'+picturePath+'">'+langName+'</option>'));     
             if(firstValue == null)
                 firstValue = langName;
         }
-
         onOptionClick(firstValue);
 
         $('#webmenu').on('change', function() {
             onOptionClick(this.value);
         });
-        
-        $('body .choose-lang-div #langs div').click(onOptionClick(this.value));
 
         try {
             $("body select[id='webmenu']").msDropDown();
@@ -62,5 +42,6 @@ $(document).ready(function(){
         }
     });
 
+    
     
 });
