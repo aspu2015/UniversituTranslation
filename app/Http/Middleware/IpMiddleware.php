298792234628e2462d18f34be1f7f03332635105
Alexander <travov_alex@mail.ru>
2019-08-27 09:16:27 +0400

    middleware

--- app/Http/Middleware/IpMiddleware.php
+++ app/Http/Middleware/IpMiddleware.php
@@ -15,14 +15,11 @@ class IpMiddleware
      */
     public function handle($request, Closure $next)
     {
-        if ($request->ip() != "172.16.2.207") {
         $ip = file_get_contents('https://api.ipify.org');
         if ($ip != "91.202.254.178") {
-            // here instead of checking a single ip address we can do collection of ips
-            //address in constant file and check with in_array function
                 return redirect('/');
             }
         return $next($request);
     }
 }
-}   
+   
